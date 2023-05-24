438dbf9737afdbc886176b04795b87ee53bf4e01
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-05-24 00:22:34 -0500

    add docker file

--- api/ListGame.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-from Game import Game
-
-
-class ListGame(BaseModel):
-    data: List[Game]
--- app/main.py
+++ app/main.py
@@ -1,16 +1,72 @@
-import uvicorn
 import pandas as pd
 import joblib
 from fastapi import FastAPI
-from ListGame import ListGame
-from Preprocessing import Preprocessing
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+from sklearn.base import TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+class Game(BaseModel):
+    id: str
+    rated: bool
+    created_at: str
+    last_move_at: str
+    turns: int
+    victory_status: str
+    winner: str
+    increment_code: str
+    white_id: str
+    white_rating: int
+    black_id: str
+    black_rating: int
+    moves: str
+    opening_eco: str
+    opening_name: str
+    opening_ply: int
+
+
+class ListGame(BaseModel):
+    data: List[Game]
+
+
+class Dropper(TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.drop(
+            ['id', 'rated', 'created_at', 'last_move_at', 'victory_status', 'winner', 'increment_code', 'white_id',
+             'white_rating', 'black_id', 'black_rating', 'moves', 'opening_eco', 'opening_name', 'opening_ply'], axis=1)
+
+
+class PredictionStandardScaler(TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        stand_sca = StandardScaler()
+
+        turns = stand_sca.fit_transform(X['turns'].values.reshape(-1, 1))
+        X['turns'] = turns
+
+        return X
+
+
+class Preprocessing:
+    final_model = Pipeline([
+        ('dropper', Dropper()),
+        ('standard_scaler', PredictionStandardScaler())
+    ])
+
 
 app = FastAPI()
 model = joblib.load("best_model.joblib")
 
 origins = [
-    "https://witty-rock-08a184410.3.azurestaticapps.net/"
+    "https://witty-rock-08a184410.3.azurestaticapps.net"
 ]
 
 app.add_middleware(
@@ -27,6 +83,11 @@ def index(data: ListGame):
     games_ids, winners = predict_results(data)
     return {'winners': winners.tolist(),
             'games ids': games_ids}
+
+
+@app.get('/')
+def index():
+    return {'winners': "hello world"}
 
 
 def predict_results(games: ListGame):
@@ -53,8 +114,3 @@ def convert_listgame_to_dataframe(games: ListGame):
                      "opening_ply"]
     df = pd.DataFrame(data=values, columns=columns_names)
     return games_ids, df
-
-
-if __name__ == '__main__':
-    uvicorn.run(app)
-# uvicorn main:app --reload
